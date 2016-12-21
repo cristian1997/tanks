01fdf7d60e51907c6b881b73455672934345d7ce
cristian1997 <cris90297@yahoo.co.uk>
2016-12-21 12:55:54 +0200

    Improved tank movement and added rotation

--- tank.cpp
+++ tank.cpp
@@ -1,80 +1,120 @@
 #include "tank.h"
 #include <iostream>
+#include <cmath>
 
-tank::tank()
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Texture * Tank::loadTexture(const char fileName[], SDL_Renderer *renderer)
 {
-    tankSurface = nullptr;
+    SDL_Surface *tempSurface;
+    SDL_Texture *retTexture;
+
+    tempSurface = IMG_Load(fileName);
+    if (tempSurface == nullptr) return nullptr;
+
+    SDL_SetColorKey(tempSurface, SDL_TRUE, SDL_MapRGB(tempSurface->format, 0xFF, 0xFF, 0xFF));
+
+    retTexture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+
+    SDL_FreeSurface(tempSurface);
+    return retTexture;
+}
+
+Tank::Tank()
+{
+    tankRenderer = nullptr;
     boolHasMoved = false;
     lastMovement = SDL_GetTicks();
 
-    xVel = 100;
-    yVel = 100;
-    dx = dy = 0.0;
+    maxTurnSpeed = 144;
+    maxSpeed = 100;
+    angle = 0.0;
 }
 
-bool tank::loadImage(const char * fileName)
+bool Tank::loadImage(const char * fileName, SDL_Renderer *renderer)
 {
-    return (tankSurface = IMG_Load(fileName));
+    tankRenderer = loadTexture(fileName, renderer);
+    if (tankRenderer == nullptr) return false;
+
+    SDL_QueryTexture(tankRenderer, nullptr, nullptr, &width, &height);
+
+    return true;
 }
 
-void tank::setPos(float x, float y)
+void Tank::setPos(float x, float y)
 {
     xPos = x;
     yPos = y;
 }
 
-bool tank::render(SDL_Surface* &dest)
+bool Tank::render(SDL_Renderer* &renderer)
 {
-    SDL_Rect pos = {(int)xPos, (int)yPos};
+    SDL_Rect pos = {(int)xPos, (int)yPos, width, height};
 
     boolHasMoved = false;
-    return !SDL_BlitSurface(tankSurface, nullptr, dest, &pos);
+    SDL_RendererFlip fp = SDL_FLIP_NONE;
+    if (SDL_RenderCopyEx(renderer, tankRenderer, nullptr, &pos, angle, nullptr, fp)) return false;
+
+    SDL_RenderPresent(renderer);
+
+    return true;
 }
 
-void tank::updatePos()
+void Tank::updatePos()
 {
     int time = SDL_GetTicks();
 
-    xPos += dx * (time - lastMovement) / 1000.0;
-    yPos += dy * (time - lastMovement) / 1000.0;
+    xPos += speed * (time - lastMovement) * cos(angle / 180.0 * acos(-1)) / 1000.0;
+    yPos += speed * (time - lastMovement) * sin(angle / 180.0 * acos(-1)) / 1000.0;
+
+    if (xPos < 0) xPos = 0;
+    if (xPos + width > SCREEN_WIDTH) xPos = SCREEN_WIDTH - width;
+
+    if (yPos < 0) yPos = 0;
+    if (yPos + height > SCREEN_HEIGHT) yPos = SCREEN_HEIGHT - height;
+
+    angle += turnSpeed * (time - lastMovement) / 1000.0;
 
     lastMovement = time;
-    boolHasMoved = (dx != 0.0 || dy != 0.0);
+    boolHasMoved = (speed || turnSpeed);
 }
 
-bool tank::hasMoved()
+bool Tank::hasMoved()
 {
     return boolHasMoved;
 }
 
-void tank::handleEvent(const SDL_Event &e)
+void Tank::handleEvent(const SDL_Event &e)
 {
     switch (e.type)
     {
         case SDL_KEYDOWN:
             switch (e.key.keysym.sym)
             {
-                case SDLK_UP: dy = -yVel; break;
-                case SDLK_DOWN: dy = yVel; break;
-                case SDLK_LEFT: dx = -xVel; break;
-                case SDLK_RIGHT: dx = xVel; break;
+                case SDLK_UP: speed = maxSpeed; break;
+                case SDLK_DOWN: speed = -maxSpeed; break;
+                case SDLK_LEFT: turnSpeed = -maxTurnSpeed; break;
+                case SDLK_RIGHT: turnSpeed = maxTurnSpeed; break;
             }
             break;
         
         case SDL_KEYUP:
             switch (e.key.keysym.sym)
             {
-                case SDLK_UP: if (dy < 0) dy = 0; break;
-                case SDLK_DOWN: if (dy > 0) dy = 0; break;
-                case SDLK_LEFT: if (dx < 0) dx = 0; break;
-                case SDLK_RIGHT: if (dx > 0) dx = 0; break;
+                case SDLK_UP: if (speed > 0) speed = 0; break;
+                case SDLK_DOWN: if (speed < 0) speed = 0; break;
+                case SDLK_LEFT: if (turnSpeed < 0) turnSpeed = 0; break;
+                case SDLK_RIGHT: if (turnSpeed > 0) turnSpeed = 0; break;
             }
             break;
     }
+
+    printf("%f\nsin = %f\ncos = %f\n", angle, sin(angle), cos(angle));
 }
 
-tank::~tank()
+Tank::~Tank()
 {
-    SDL_FreeSurface(tankSurface);
-    tankSurface = nullptr;
+    SDL_DestroyTexture(tankRenderer);
+    tankRenderer = nullptr;
 }
